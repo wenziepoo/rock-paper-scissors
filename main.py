10884cbdbf6c7f0367a836d0b817fc33dfc3e460
wenziepoo <123613375+wenziepoo@users.noreply.github.com>
2023-02-02 13:57:16 -0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 items = ['A','B','C']
 counter  = [0,0]
 choice = ''
+
+
 def on_gesture_shake():
     game()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def on_button_pressed_a():
     counter = [0,0]
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def game():
     global items
-    radio.set_group(7)
     choice = items[randint(0,2)]
+    radio.set_group(8)
     radio.send_string(choice)
+    basic.pause(500)
     score = str(counter[0]) + ' - ' + str(counter[1])
 
-def on_received_string2(receivedString):
-    pass
-radio.on_received_string(on_received_string2)
+
 
 def on_received_string(receivedString):
     global choice
     global counter
-    def on_received_string2(receivedString):
-        pass
     if choice == 'A':
         if receivedString == 'C':
             counter[1] = counter[1]+1
         elif receivedString != choice:
             counter[0] = counter[0]+1
+
     elif choice == 'B':
         if receivedString == 'A':
             counter[1] = counter[1]+1
         elif receivedString != choice:
                 counter[0] = counter[0]+1
+                
     elif choice == "C":
         if receivedString == 'B':
             counter[1] = counter[1]+1
         elif receivedString != choice:
             counter[0] = counter[0]+1
-    radio.on_received_string(on_received_string)
+radio.on_received_string(on_received_string)
--- main.ts
+++ main.ts
@@ -9,12 +9,37 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 function game() {
     
-    radio.setGroup(7)
     let choice = items[randint(0, 2)]
+    radio.setGroup(8)
     radio.sendString(choice)
+    basic.pause(500)
     let score = "" + counter[0] + " - " + ("" + counter[1])
 }
 
-radio.onReceivedString(function on_received_string2(receivedString: string) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+    
+    if (choice == "A") {
+        if (receivedString == "C") {
+            counter[1] = counter[1] + 1
+        } else if (receivedString != choice) {
+            counter[0] = counter[0] + 1
+        }
+        
+    } else if (choice == "B") {
+        if (receivedString == "A") {
+            counter[1] = counter[1] + 1
+        } else if (receivedString != choice) {
+            counter[0] = counter[0] + 1
+        }
+        
+    } else if (choice == "C") {
+        if (receivedString == "B") {
+            counter[1] = counter[1] + 1
+        } else if (receivedString != choice) {
+            counter[0] = counter[0] + 1
+        }
+        
+    }
     
 })
